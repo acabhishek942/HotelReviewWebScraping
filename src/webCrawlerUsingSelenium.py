776d18cb6d24301faefe6c2a85009ee97ee55091
Abhishek Choudhary <acabhishek942@gmail.com>
2018-06-08 23:40:24 +0530

    now parsing the days in the format that MMT requires

--- src/webCrawlerUsingSelenium.py
+++ src/webCrawlerUsingSelenium.py
@@ -6,3 +6,22 @@ import datetime
 import os
 import time
 import pandas as pd
+
+url = 'https://www.makemytrip.com/hotels/'
+
+# hard-coded hotel name
+hotel_name = 'The Chancery Pavilion'
+
+# perform search relative to current date
+current_time = datetime.datetime.now()
+current_time.strftime("%m/%d/%Y")
+
+# some parameters to customize the search
+nights_stay = 2
+weeks_from_now_to_look = 13
+how_many_weeks_to_check = 4
+
+for week in range(how_many_weeks_to_check):
+    start_date = current_time + datetime.timedelta(days=week*7) + datetime.timedelta(days=weeks_from_now_to_look*7)
+    end_date = start_date + datetime.timedelta(days=nights_stay)
+    print(start_date.strftime("%d %b, %a") + ' to ' + end_date.strftime("%d %b, %a"))
